52346ca4c58d8b364576d68b610bcacb860b74f4
KyleMahan <kmahan95@gmail.com>
2021-02-08 22:58:40 -0500

    1

--- discord_bot/706_Discord_Bot.py
+++ discord_bot/706_Discord_Bot.py
@@ -30,8 +30,21 @@ async def on_ready():
 
     # Add in additional commands now
 
-    #Example command to print server members
+    # Example command to print server members
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+
+    # Trying to list the channels
+    channels = '\n - '.join([channel.name for channel in guild.channel])
+
+"""
+# Trying a new client event to monitor messages in the dev channel
+@client.event
+async def on_message(message)
+    if message.channel != "bot-dev"
+        return
+    
+    elif
+"""
 
 client.run(TOKEN)
