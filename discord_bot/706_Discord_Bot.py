4674f96fc501f204b2902663b36b6da3dc1fe12c
KyleMahan <kmahan95@gmail.com>
2021-02-08 22:16:46 -0500

    First Discord Program

--- discord_bot/706_Discord_Bot.py
+++ discord_bot/706_Discord_Bot.py
@@ -4,3 +4,26 @@ This is a discod bot for use in the 706 discord.
 Author: Kyle Mahan
 """
 
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+client.run(TOKEN)
